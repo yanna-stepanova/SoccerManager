4e3385bc0d9c5ed3462a62abd38ed58cde9c6152
Yana Stepanova <yanna.stepanova@gmail.com>
2025-01-07 17:25:26 +0200

    initialization

---
+++ src/main/java/yanna/stepanova/SoccerManagerApplication.java
@@ -0,0 +1,13 @@
+package yanna.stepanova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerManagerApplication.class, args);
+	}
+
+}
