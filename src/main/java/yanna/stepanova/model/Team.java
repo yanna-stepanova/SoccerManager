103c6e518226f39ed9ec0ec22f26620a749610ad
Yana Stepanova <yanna.stepanova@gmail.com>
2025-01-08 00:59:01 +0200

    added model level

---
+++ src/main/java/yanna/stepanova/model/Team.java
@@ -0,0 +1,40 @@
+package yanna.stepanova.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "teams")
+@SQLDelete(sql = "UPDATE teams SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction("is_deleted = FALSE")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private BigDecimal balance;
+    @Column(nullable = false)
+    private float commission;
+    @OneToMany(mappedBy = "team")
+    private Set<Player> playerSet = new HashSet<>();
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+}
